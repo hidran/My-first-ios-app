1555a97f7d639a6bdabea1b400b892df21aac5fb
Hidran Arias <hidran@gmail.com>
2022-10-08 18:17:37 +0200

    Adds label and ban action

--- My first ios app/ViewController.swift
+++ My first ios app/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var message: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickedSaveBtn(_ sender: Any) {
+        
+        message.text = "Ciao Mondo";
+        
+        print("Clicked Saved")
+        
+    }
+    
 }
 
